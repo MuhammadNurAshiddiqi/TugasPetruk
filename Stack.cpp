db62ff9d7d9c1362c958153cbb47bc7d7477ab72
MuhammadNurAshiddiqi <48676131+MuhammadNurAshiddiqi@users.noreply.github.com>
2019-04-14 21:20:18 +0700

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -12,7 +12,6 @@ struct Element // Struktur Element
     int data;
     Element *link;
 };
-
 Element *top = NULL;//pointer atas untuk melacak bagian atas tumpukan 
 bool isEmpty()// Berfungsi untuk memeriksa apakah tumpukan kosong atau tidak
 {
@@ -32,8 +31,7 @@ void push(int value)// Berfungsi untuk memasukkan elemen dalam tumpukan
 
 void pop ()// Berfungsi untuk menghapus elemen dari stack
 {
-	if ( isEmpty() )
-		cout<<"Stack Kosong";
+	if ( isEmpty() );
 	else
 {
 	cout<<"Stack Telah Di Pop\n";
@@ -74,32 +72,36 @@ int main()// Fungsi utama
 
 
  while( flag == 1){
-cout<<"\n==================\n";
+cout<<"\n================\n";
 cout<<"1.Push \n";
 cout<<"2.Pop \n";
 cout<<"3.showTop \n";
 cout<<"4.Keluar \n";
-cout<<"====================\n";
+cout<<"================\n";
 cout<<"Pilih : ";cin>>pil;
-cout<<"====================";
+cout<<"================\n";
 cout<<endl<<endl;
 	switch (pil)//Program Menu menggunakan Switch
 	{
-	case 1: cout<<"Masukan Nilai: ";
+	case 1: system("clear");
+			cout<<"Masukan Nilai: ";
 			cin>>value;
 			push(value);
-			cout << "====List Stack====\n";
+			cout << "===List Stack===\n";
 			printStackList();
 			break;
-	case 2: pop();
-			cout << "====List Stack====\n";
+	case 2: system("clear");
+			pop();
+			cout << "===List Stack===\n";
 			printStackList();
 			break;
-	case 3: showTop();
-			cout << "====List Stack====\n";
+	case 3: system("clear");
+			showTop();
+			cout << "===List Stack===\n";
 			printStackList();
 			break;
-	case 4: flag = 0;
+	case 4: system("clear");
+			flag = 0;
 			cout << endl << endl;
 			break;
          
